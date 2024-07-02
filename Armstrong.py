f119ae0d31c88ea2cebdea28eaa552187e1d2634
pranav24547 <pranavthanavel@gmail.com>
2024-07-02 22:17:09 +0530

    Updated Armstrong

--- Armstrong.py
+++ Armstrong.py
@@ -1,13 +1,13 @@
-Number=int(input("Enter the number:"))
-Numstr=str(Number)
-length=len(Numstr)
-sum=0
-for i in Numstr:
-    sum +=int(i)**length
-if sum == Number:
-    print(Number,"is an Armstrong Number")
-else:
-    print(Number,"is not an amstrong Number")
-
-
-
+n=int(input("Enter the length of the list:"))
+ans=0
+for i in range(n): 
+    Number=int(input())
+    Numstr=str(Number)
+    length=len(Numstr)
+    sum=0
+    for i in Numstr:
+        #1,5,3
+        sum +=int(i)**length
+    if sum == Number:
+        ans +=1
+print(ans)
